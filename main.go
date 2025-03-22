ab176aedbba8d89ef2ac230c0d35aca16fadd5f9
suppur <zi213@hotmail.com>
2025-03-21 23:17:23 -0400

    added xml structs + feed fetcher

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerListUsers)
+	cmds.register("agg", handlerAgg)
 
 	usrInput := os.Args
 	if len(usrInput) < 2 {
